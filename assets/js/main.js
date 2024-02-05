73a38d05ccfea0131d692e1691e5fda3a0694b5c
Ragastra07 <haryo.accn.7@gmail.com>
2024-02-05 12:52:19 +0700

    header & navbar section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,32 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+        navToggle = document.getElementById('nav-toggle'),
+        navClose = document.getElementById('nav-close')
+
+    // menu show
+
+if(navToggle){
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+if(navClose){
+    navClose.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
 
 
 /*=============== REMOVE MENU MOBILE ===============*/
+
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () => {
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 
 /*=============== ADD BLUR HEADER ===============*/
